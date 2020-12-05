bdd08c9962c3c4c2387b473de00f11abb4882368
zachang <zachangdawuda@gmail.com>
2020-12-05 14:47:15 +0100

    handle requests error

--- cinema/service.py
+++ cinema/service.py
@@ -1,21 +1,40 @@
 import requests
 
-MOVIE_URL = 'https://ghibliapi.herokuapp.com/films/'
-CAST_URL = 'https://ghibliapi.herokuapp.com/people/'
+
+from requests_toolbelt import sessions
+from urllib3.util.retry import Retry
+
+from cinema.timeout_adapter import TimeoutHTTPAdapter
+
+retry_strategy = Retry(
+    total=3,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"]
+)
+
+MOVIE_URL = '/films/'
+CAST_URL = '/people/'
+http = sessions.BaseUrlSession(base_url='https://ghibliapi.herokuapp.com')
+
+# Mount it for both http and https usage
+adapter = TimeoutHTTPAdapter(timeout=5, max_retries=retry_strategy)
+http.mount("https://", adapter)
 
 
 def get_movies():
     """All movies api call"""
-    response = requests.get(MOVIE_URL)
-    movies = response.json() if response.status_code == 200 else []
+    response = http.get(MOVIE_URL)
+    movies = response.json() if response.status_code == requests.codes.ok else []
     return movies
 
 
 def get_movie(movie_id):
-    """Specific movie api call"""
-    movie_response = requests.get(f'{MOVIE_URL}{movie_id}')
-    if movie_response.status_code == 200:
-        people_response = requests.get(CAST_URL)
+    """Specific movie api call
+    :param movie_id: The specific movie resource.
+    """
+    movie_response = http.get(f'{MOVIE_URL}{movie_id}')
+    if movie_response.status_code == requests.codes.ok:
+        people_response = http.get(CAST_URL)
         movie = movie_response.json()
         people = people_response.json()
         casts = (cast['name'] for cast in people if movie_id in ','.join(cast['films']))
--- cinema/views.py
+++ cinema/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.views.decorators.http import require_http_methods
 
+import requests
 from cinema.service import get_movies, get_movie
 
 
@@ -16,16 +17,19 @@ def home(request):
 @require_http_methods(['GET'])  # with DRF this would be @api_view(['GET'])
 def films(request):
     """Resource view for all movies"""
-    movies_list = get_movies()
-    page = request.GET.get('page', 1)
-    paginator = Paginator(movies_list, 10)
-
     try:
+        movies_list = get_movies()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(movies_list, 10)
         movies = paginator.page(page)
     except PageNotAnInteger:
         movies = paginator.page(1)
     except EmptyPage:
         movies = paginator.page(paginator.num_pages)
+    except requests.exceptions.ConnectionError:
+        return render(request, 'cinema/network_failure.html')
+    except requests.exceptions.Timeout:
+        return render(request, 'cinema/timeout.html')
 
     context = {'movies': movies}
     return render(request, 'cinema/movies.html', context)
@@ -35,5 +39,12 @@ def films(request):
 @require_http_methods(['GET'])  # with DRF this would be @api_view(['GET'])
 def film(request, movie_id):
     """Resource view for a single movie"""
-    movie = get_movie(movie_id)
-    return render(request, 'cinema/movie.html', {'movie': movie})
+    try:
+        movie = get_movie(movie_id)
+        context = {'movie': movie}
+    except requests.exceptions.ConnectionError:
+        return render(request, 'cinema/network_failure.html')
+    except requests.exceptions.Timeout:
+        return render(request, 'cinema/timeout.html')
+
+    return render(request, 'cinema/movie.html', context)
